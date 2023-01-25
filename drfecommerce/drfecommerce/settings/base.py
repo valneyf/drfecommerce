1b76208ecbeb8ac07759eaef2ad5e44c47df495a
Valney Filho <valneyf.dev@gmail.com>
2023-01-24 21:29:09 -0300

    Instalado djangorestframework

--- drfecommerce/drfecommerce/settings/base.py
+++ drfecommerce/drfecommerce/settings/base.py
@@ -19,6 +19,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # external packages
+    'rest_framework',
+
+    # internal apps
+
 ]
 
 MIDDLEWARE = [
@@ -91,3 +97,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {}
